b1b205498b7f2c915c1905d797eaa16ffd327162
adinhobl <andrewdinhobl@gmail.com>
2018-12-27 20:40:05 -0600

    app.py allows prediction page to display multiple times with new images

--- app.py
+++ app.py
@@ -15,10 +15,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
+    # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -29,25 +27,29 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            print(filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('predict', filename=filename))
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        return upload_file()
     return render_template('upload.html')
 
 @app.route('/<filename>', methods=['POST','GET'])
 def predict(filename):
-    #make prediction
+    # make prediction
+    
+    
+    
     result = "cat"
 
-    # render template and display image
-    # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     
-    #upload box
-
-    
+    # display page & handle upload
+    if request.method == 'POST':
+        return upload_file()
     return render_template('result.html', user_image=filename, result=result)
-    # return redirect(url_for('predict', filename=filename))
-    # return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 
 
